7876d952d22b983c5f5d27af5d87759ea6ed4727
Mahima <mahimagarg832@gmail.com>
2021-06-02 18:34:33 +0530

    gh

--- src/Component/Allcatag.js
+++ src/Component/Allcatag.js
@@ -1,24 +1,18 @@
 import "./Custom.css"
 import Image1 from "./Images/image1.jpg"
-import Image2 from "./Images/a.jpg"
-import Image3 from "./Images/a.jpg"
-import Image from "./Images/a.jpg"
-import Image from "./Images/a.jpg"
-import Image from "./Images/a.jpg"
-import Image from "./Images/a.jpg"
-import Image from "./Images/a.jpg"
+import Image2 from "./Images/image2.jpg"
+import Image3 from "./Images/image3.jpg"
+import Image4 from "./Images/image4.jpg"
+
 
 function Allcatag (){
     return(
 <div className="head-clr head-pd  allcatg-h   ">
 <img src={Image1} className="poster"  alt="not available"  />
 <img src={Image2} className="poster"  alt="not available"  />
-<img src={Image} className="poster"  alt="not available"  />
-<img src={Image} className="poster"  alt="not available"  />
-<img src={Image} className="poster"  alt="not available"  />
-<img src={Image} className="poster"  alt="not available"  />
-<img src={Image} className="poster"  alt="not available"  />
-<img src={Image} className="poster"  alt="not available"  />
+<img src={Image3} className="poster"  alt="not available"  />
+<img src={Image4} className="poster"  alt="not available"  />
+
 
 </div>
     );
